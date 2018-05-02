cf996192c4077bd9cce590b742a04c0c7ee5a16c
travis88 <hellsballs99@gmail.com>
2018-05-02 16:37:52 +0300

    sparkline added

--- src/containers/weather_list.js
+++ src/containers/weather_list.js
@@ -1,11 +1,18 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import Chart from '../components/chart';
 
 class WeatherList extends Component {
     renderWeather(cityData) {
+        const temps = cityData.list.map(weather => 
+            (weather.main.temp - 273.15).toFixed(2));
+        
         return (
             <tr key={cityData.city.id}>
                 <td>{cityData.city.name}</td>
+                <td>
+                    <Chart data={temps} color="orange" />
+                </td>
             </tr>
         );
     }
