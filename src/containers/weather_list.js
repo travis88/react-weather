ae44a8b97991050360a8a57bd210f0ac32145341
travis88 <hellsballs99@gmail.com>
2018-05-02 16:12:53 +0300

    showing cities

---
+++ src/containers/weather_list.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+class WeatherList extends Component {
+    renderWeather(cityData) {
+        return (
+            <tr key={cityData.city.id}>
+                <td>{cityData.city.name}</td>
+            </tr>
+        );
+    }
+
+    render() {
+        return (
+            <table className="table table-hover">
+                <thead>
+                    <tr>
+                        <th>Город</th>
+                        <th>Температура</th>
+                        <th>Давление</th>
+                        <th>Влажность</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {this.props.weather.map(this.renderWeather)}
+                </tbody>
+            </table>
+        );
+    }
+}
+
+function mapStateToProps({ weather }) {
+    return { weather };
+}
+
+export default connect(mapStateToProps)(WeatherList);
